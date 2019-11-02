798b4236bbdca7ddca6a48047d8c943faf130dd4
Sunday <s5935512004@phuket.psu.ac.th>
2019-11-02 10:29:17 +0700

    Add Logo :art:
    - :heavy_plus_sign: next-image @zeit/next-sass

--- components/nav.js
+++ components/nav.js
@@ -1,31 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Link from 'next/link'
 
-const Nav = () => (
-  <nav>
-    <div>
-      <Link href='/'>
-        <h2>STOCK</h2>
-      </Link>
-    </div>
-    <div><ul>
-      <li>
-        <Link href='/'>
-          <a>Home</a>
-        </Link>
-        <Link href='/manage'>
-          <a>Manage</a>
-        </Link>
-        <Link href='/product'>
-          <a>Product</a>
-        </Link>
-        <Link href='/about'>
-          <a>About</a>
-        </Link>
-      </li>
-    </ul></div>
+const Nav = () => {
 
-    <style jsx global>{`
+  const [logo, setLogo] = useState('/static/img/Logo.PNG')
+
+  return (
+    <nav>
+      <div>
+        <Link href='/'>
+          <img className="logo" src={logo}></img>
+        </Link>
+      </div>
+      <div><ul>
+        <li>
+          <Link href='/'>
+            <a>Home</a>
+          </Link>
+          <Link href='/manage'>
+            <a>Manage</a>
+          </Link>
+          <Link href='/product'>
+            <a>Product</a>
+          </Link>
+          <Link href='/about'>
+            <a>About</a>
+          </Link>
+        </li>
+      </ul></div>
+
+      <style jsx global>{`
         @font-face {
           font-family: Roboto;
           src: url(/static/font/Roboto-Regular.ttf);
@@ -36,7 +40,7 @@ const Nav = () => (
           font-family: 'Roboto';
         }
       `}</style>
-    <style jsx>{`
+      <style jsx>{`
       :global(body) {
         font-family: -apple-system, BlinkMacSystemFont, Avenir Next, Avenir,
           Helvetica, sans-serif;
@@ -47,6 +51,11 @@ const Nav = () => (
         justify-content: space-between;
         align-items: center;
         box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
+      }
+      .logo {
+        cursor: pointer;
+        margin: 0 20px;
+        height: 35px;
       }
       ul {
         display: flex;
@@ -74,7 +83,8 @@ const Nav = () => (
         color: #067df7;
       }
     `}</style>
-  </nav>
-)
+    </nav>
+  )
+}
 
 export default Nav
